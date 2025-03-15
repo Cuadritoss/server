d1b636b07185578857535d747d30e7c8844d3cac
Cuadritoss <bakeshopcuadrito@gmail.com>
2025-03-15 21:37:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ let db;
 const connectDB = async () => {
     try {
         db = await mysql.createConnection({
-            host: "194.59.164.63",
+            host: "srv1859.hstgr.io",
             user: "u471919822_cuadritodb",
             password: "Cuadrito_123",
             database: "u471919822_cuadritoDB",
